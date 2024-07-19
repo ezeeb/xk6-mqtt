72e610ee851c17cf53e7cc4f8808e21b122cc251
ezeeb <elmar.zeeb@vreeda.com>
2024-07-19 11:44:42 +0200

    add go path to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$(go env GOPATH)/bin:$PATH
+
 xk6 build v0.52.0 \
   --replace google.golang.org/genproto@v0.0.0-20210226172003-ab064af71705=google.golang.org/genproto@v0.0.0-20230526161137-0005af68ea54 \
   --replace google.golang.org/genproto@v0.0.0-20230410155749-daa745c078e1=google.golang.org/genproto@v0.0.0-20230526161137-0005af68ea54 \
